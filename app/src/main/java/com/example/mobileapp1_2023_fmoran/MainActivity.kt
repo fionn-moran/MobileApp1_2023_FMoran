72ccda4729c4c3fe1d70098263b5156cc7d1c8e9
fionnmoran <fionnmoran13@gmail.com>
2023-03-03 14:51:38 +0000

    Update activity_main.xml, add relative layout

--- app/src/main/java/com/example/mobileapp1_2023_fmoran/MainActivity.kt
+++ app/src/main/java/com/example/mobileapp1_2023_fmoran/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.mobileapp1_2023_fmoran
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import timber.log.Timber
+import timber.log.Timber.i
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Timber.plant(Timber.DebugTree())
+
+        i("Main Activity started...")
+
+
     }
 }
