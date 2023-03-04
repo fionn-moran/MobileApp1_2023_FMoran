3b19f8a1a3018dc7ec2bcedd9ff7b320e7d953cb
fionnmoran <fionnmoran13@gmail.com>
2023-03-04 16:44:00 +0000

    Main activity now calling array list created in Main App

--- app/src/main/java/com/example/mobileapp1_2023_fmoran/activities/MainActivity.kt
+++ app/src/main/java/com/example/mobileapp1_2023_fmoran/activities/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Toast
 import com.example.mobileapp1_2023_fmoran.R
 import com.example.mobileapp1_2023_fmoran.databinding.ActivityMainBinding
+import com.example.mobileapp1_2023_fmoran.main.MainApp
 import com.example.mobileapp1_2023_fmoran.models.MainActivityModel
 import timber.log.Timber
 import timber.log.Timber.i
@@ -13,23 +14,23 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     var newBooking = MainActivityModel()
-    val bookings = ArrayList<MainActivityModel>()
+    var app : MainApp? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        app = application as MainApp
+        i("Main Activity Started")
         binding.btnAdd.setOnClickListener() {
             newBooking.title = binding.bookingTitle.text.toString()
             newBooking.description = binding.description.text.toString()
             if (newBooking.title.isNotEmpty()) {
-
-                bookings.add(newBooking.copy())
+                app!!.bookings.add(newBooking.copy())
                 i("add Button Pressed: ${newBooking.title}")
-                for (i in bookings.indices)
-                { i("Booking[$i]:${this.bookings[i]}") }
+                for (i in app!!.bookings.indices)
+                { i("Booking[$i]:${this.app!!.bookings[i]}") }
             }
             else {
                 Toast.makeText(this,"Please Enter a title", Toast.LENGTH_LONG)
--- app/src/main/java/com/example/mobileapp1_2023_fmoran/main/MainApp.kt
+++ app/src/main/java/com/example/mobileapp1_2023_fmoran/main/MainApp.kt
@@ -1,10 +1,13 @@
 package com.example.mobileapp1_2023_fmoran.main
 
 import android.app.Application
+import com.example.mobileapp1_2023_fmoran.models.MainActivityModel
 import timber.log.Timber
 import timber.log.Timber.i
 
 class MainApp : Application() {
+
+    val bookings = ArrayList<MainActivityModel>()
 
     override fun onCreate() {
         super.onCreate()
