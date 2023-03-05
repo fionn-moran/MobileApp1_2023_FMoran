50f1c6044d5c07a6e4f930f1ed6869af33d5b7ee
fionnmoran <fionnmoran13@gmail.com>
2023-03-05 02:26:49 +0000

    Update booking adapter

--- app/src/main/java/com/example/mobileapp1_2023_fmoran/adapters/BookingAdapter.kt
+++ app/src/main/java/com/example/mobileapp1_2023_fmoran/adapters/BookingAdapter.kt
@@ -6,7 +6,11 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.mobileapp1_2023_fmoran.databinding.BookingCardViewBinding
 import com.example.mobileapp1_2023_fmoran.models.MainActivityModel
 
-class BookingAdapter constructor(private var placemarks: List<MainActivityModel>) :
+interface BookingListener {
+    fun OnBookingClick(booking: MainActivityModel)
+}
+
+class BookingAdapter constructor(private var bookings: List<MainActivityModel>, private val listener: BookingListener) :
     RecyclerView.Adapter<BookingAdapter.MainHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainHolder {
@@ -17,18 +21,19 @@ class BookingAdapter constructor(private var placemarks: List<MainActivityModel>
     }
 
     override fun onBindViewHolder(holder: MainHolder, position: Int) {
-        val placemark = placemarks[holder.adapterPosition]
-        holder.bind(placemark)
+        val booking = bookings[holder.adapterPosition]
+        holder.bind(booking, listener)
     }
 
-    override fun getItemCount(): Int = placemarks.size
+    override fun getItemCount(): Int = bookings.size
 
     class MainHolder(private val binding : BookingCardViewBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(placemark: MainActivityModel) {
-            binding.title.text = placemark.title
-            binding.description.text = placemark.description
+        fun bind(booking: MainActivityModel, listener: BookingListener) {
+            binding.title.text = booking.title
+            binding.description.text = booking.description
+            binding.root.setOnClickListener { listener.OnBookingClick(booking) }
         }
     }
 }
