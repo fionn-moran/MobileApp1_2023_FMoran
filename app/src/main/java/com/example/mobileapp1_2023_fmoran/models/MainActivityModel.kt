3ac9daf277e1e5c953dfd344561c404221e701b5
fionnmoran <fionnmoran13@gmail.com>
2023-03-05 11:48:08 +0000

    Implement parcelize - main model

--- app/src/main/java/com/example/mobileapp1_2023_fmoran/models/MainActivityModel.kt
+++ app/src/main/java/com/example/mobileapp1_2023_fmoran/models/MainActivityModel.kt
@@ -1,3 +1,8 @@
 package com.example.mobileapp1_2023_fmoran.models
 
-data class MainActivityModel(var title: String = "", var description: String = "")
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+
+@Parcelize
+data class MainActivityModel(var title: String = "", var description: String = "") : Parcelable
